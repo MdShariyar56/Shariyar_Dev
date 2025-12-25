3eff1cfb18fd56f37ee2794f408312bcc7239519
MdShariyar56 <shahriyarkobir2005@gmail.com>
2025-12-25 13:36:20 +0600

    Added a contact section edit

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="bg-blue-400 py-8 px-4"
+      className=" py-8 px-4"
     >
       <div className="max-w-7xl mx-auto">
         {/* Section Header */}
